377f3a69635af30950446352b99d51574f0f5756
vanmong <taeprojh@naver.com>
2023-01-31 17:36:35 +0900

    0131

---
+++ js/project.js
@@ -0,0 +1,9 @@
+const ibtn = document.querySelector('.fa-plus');
+const dropmenu = document.querySelector('.dropmenu');
+
+ibtn.addEventListener('click', () => 
+ibtn.classList.toggle('active')
+);
+ibtn.addEventListener('click', () => 
+dropmenu.classList.toggle('active')
+);
