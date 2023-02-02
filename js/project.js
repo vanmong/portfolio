d3188f8f8ec2e6236ab3875b93ec4103dea63094
vanmong <taeprojh@naver.com>
2023-02-02 15:31:37 +0900

    0202

--- js/project.js
+++ js/project.js
@@ -7,3 +7,13 @@ ibtn.classList.toggle('active')
 ibtn.addEventListener('click', () => 
 dropmenu.classList.toggle('active')
 );
+/* 
+const scrollY = document.querySelector(scrollY);
+const article = document.querySelector('.article')
+if (scrollY) {
+  scrollY >= 306 (
+    article.classList.add('list')
+  )
+} else {
+  
+}; */
