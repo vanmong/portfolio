73ee088ad70081b3cfe6c4ea6d9592eb2b81f1d3
vanmong <taeprojh@naver.com>
2023-02-02 17:44:58 +0900

    0202

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-ScrollReveal().reveal('.kategorie');
+AOS.init();
+
+// 페이지전환효과
+jQuery(function($) {
+    $("body").css("display", "none");
+    $("body").fadeIn(1000);
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage);
+    });
+    function redirectPage() {
+    window.location = linkLocation;
+    }
+});
--- js/project.js
+++ js/project.js
@@ -7,7 +7,9 @@ ibtn.classList.toggle('active')
 ibtn.addEventListener('click', () => 
 dropmenu.classList.toggle('active')
 );
+
 /* 
+내가싸지른 코드 잔재
 const scrollY = document.querySelector(scrollY);
 const article = document.querySelector('.article')
 if (scrollY) {
@@ -17,3 +19,18 @@ if (scrollY) {
 } else {
   
 }; */
+
+
+// 페이지전환효과
+jQuery(function($) {
+  $("body").css("display", "none");
+  $("body").fadeIn(1000);
+  $("a.transition").click(function(event){
+      event.preventDefault();
+      linkLocation = this.href;
+      $("body").fadeOut(1000, redirectPage);
+  });
+  function redirectPage() {
+  window.location = linkLocation;
+  }
+});
